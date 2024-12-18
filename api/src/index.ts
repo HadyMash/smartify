6cdddad8cd28fb2f3a8f504bf69c66f06633546b
Hady Mashhour <hady.mashhour@gmail.com>
2024-12-18 20:42:58 +0400

    added a routes and middleware folder with examples of how to use both

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import logMiddleware from './middleware/log.middleware';
 
 dotenv.config();
 
@@ -9,12 +10,16 @@ const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(logMiddleware);
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// This is how you include your router
+// router.use('/template', templateRouter);
 
 app.use('/api', router);
 
